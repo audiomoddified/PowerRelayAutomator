36a6fcbff32b2defbe7de1993390cf5634d9f2a8
audiomodder <audiomodder@gmail.com>
2026-03-16 18:04:31 -0500

    Minro Cleanup

--- src/main/java/net/audiomodder/powerrelayautomator/HelloController.java
+++ src/main/java/net/audiomodder/powerrelayautomator/HelloController.java
@@ -3,7 +3,6 @@ package net.audiomodder.powerrelayautomator;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class HelloController {
